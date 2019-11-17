07b26f138464889455264cad89cef75317679206
vasilijus <vasiliokas@gmail.com>
2019-11-17 15:23:31 +0000

    plain blog done

--- djangoapp/posts/models.py
+++ djangoapp/posts/models.py
@@ -6,3 +6,7 @@ class Posts(models.Model):
     title   = models.CharField(max_length=100)
     body    = models.TextField()
     created_at = models.DateTimeField(default=datetime.now, blank=True)
+    def __str__(self):
+        return str(self.id)+": "+self.title # Parse ID from INT to STR
+    class Meta:
+        verbose_name_plural = "Posts"
